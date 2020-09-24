b6124de173c4682ec59586b8188c8ddb831ebb75
charliepatronr <54086538+charliepatronr@users.noreply.github.com>
2020-09-23 19:29:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+
+
+const map = function(arr, callback){
+    let newArr = []
+
+    for(let i=0; i < arr.length; i++){
+        newArr.push(callback(arr[i]))
+    }
+    return newArr
+}
+function reduce(arr, callback, starting){
+    let target = (!!starting) ? starting : arr[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < arr.length; i++) {
+        console.log(arr[i], i)
+        target = callback(arr[i], target)
+    }
+  
+    return target;
+  }
